c2d308aad1276537c968c956c346435f4910430b
sanjay-03-10 <jacksparrow31003@gmail.com>
2025-02-04 20:33:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -173,4 +173,4 @@ def home():
     return "🚀 WhatsApp Bot is Running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
